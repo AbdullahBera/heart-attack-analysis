4f5f153098a3960971641e795fd466244c115c67
AbdullahBera <kucukabdullahbera@gmail.com>
2025-03-19 17:50:57 -0700

    data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from typing import Tuple
+
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """
+    Load and preprocess the cardiovascular dataset
+
+    Parameters:
+    file_path (str): Path to the CSV file
+
+    Returns:
+    pd.DataFrame:
+        - Processed DataFrame with age converted from days to years
+        - Outliers dropped from ap_hi and ap_lo
+        - Data types converted to category
+        - Drop redundant columns
+    """
+    df = pd.read_csv(file_path)
+
+    # Convert age from days to years
+    df["age"] = (df["age"] / 365).round().astype(int)
+
+    # Remove outliers
+    remove_outliers_ap_hi = (
+        (df["ap_hi"] > 250)
+        | (df["ap_hi"] < 25)
+        | (df["ap_lo"] > 200)
+        | (df["ap_lo"] < 20)
+    )
+    df = df[~remove_outliers_ap_hi]
+
+    # Convert categorical data types into category
+    categorical_col = ["cholesterol", "gluc", "smoke", "alco", "active", "cardio"]
+    df[categorical_col] = df[categorical_col].astype("category")
+
+    # Create BMI column
+    df["bmi"] = (df["weight"] / ((df["height"] / 100) ** 2)).round(2)
+
+    # Drop Columns
+    df.pop["id"]
+    df.pop["weight"]
+    df.pop["height"]
+
+    return df
+
+
+def split_data(
+    df: pd.DataFrame,
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """
+    Split the dataset into training and testing sets
+
+    Parameters:
+    pd.DataFrame
+
+    Returns:
+    Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+        X_train, X_test, y_train, y_test
+    """
+
+    X = df.drop(columns=["cardio"])
+    y = df["cardio"]
+
+    return train_test_split(X, y, test_size=0.2, random_state=19)
+
+
+if __name__ == "__main__":
+    """
+    Save splitted training and testing files into '../data/"
+    """
+    df = pd.read_csv("../data/cardio_train.csv", delimiter=";")
+    X_train, X_test, y_train, y_test = split_data(df)
+    X_train.to_csv("../data/X_train.csv", index=False)
+    X_test.to_csv("../data/X_test.csv", index=False)
+    y_train.to_csv("../data/y_train.csv", index=False)
+    y_test.to_csv("../data/y_test.csv", index=False)
