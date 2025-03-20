641d1c002c7208fe796ca3061908d2be166b5b46
AbdullahBera <kucukabdullahbera@gmail.com>
2025-03-20 01:27:21 -0700

    streamlit app

---
+++ src/model_explainability.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import shap
+import joblib
+import matplotlib.pyplot as plt 
+from sklearn.ensemble import RandomForestClassifier
+
+
+def explain_model(model: RandomForestClassifier, X_test: pd.DataFrame) -> str:
+    """
+    Generates a SHAP summary plot to explain the model's predictions.
+
+    Parameters:
+    model_path : str
+        The file path to the trained model (e.g., "best_model_rf.pkl").
+    X_test : pd.DataFrame
+        The feature matrix for test data.
+
+    Returns:
+    plot_path : str
+        The file path where the SHAP summary plot is saved.
+    """
+    explainer = shap.Explainer(model)
+    shap_values = explainer(X_test)
+
+    shap.summary_plot(shap_values, X_test, show=False)
+    plt.savefig("../models/shap_summary.png")
+
+
+if __name__ == "__main__":
+
+    model = joblib.load("../models/best_model_rf.pkl")
+    X_test = pd.read_csv("../data/X_test.csv")
+
+    explain_model(model, X_test)
---
+++ src/model_training.py
@@ -0,0 +1,66 @@
+import pandas as pd 
+import numpy as np
+import joblib
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.metrics import classification_report
+
+
+def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> RandomForestClassifier:
+    '''
+    Train data using Random Forest using cross-validation to 
+    find the best hyperparameters  
+
+    Parameters: 
+    X_train: pd.DataFrame
+        The training feature matrix (row=samples, columns=features)
+
+    y_train: pd.Series
+        The target variable (1D array of class labels)
+
+    Returns: 
+    best_model = RandomForestClassifier
+        The best-trained Random Forest model based on cross validation
+    '''
+
+    # Define the hyperparameters for tuning
+    param_grid = {
+        'n_estimators': [50, 100, 200],
+        'max_depth': [10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'bootstrap': [True, False]
+    }
+
+    # Train the model using Random Forest and GridSearchCV
+    rf = RandomForestClassifier(random_state=19)
+    grid_search = GridSearchCV(rf, 
+                               param_grid, 
+                               cv=5, 
+                               n_jobs=-1, 
+                               verbose=2)
+
+    # Fit the model using Grid search 
+    grid_search.fit(X_train, y_train)
+
+    print(f"Best Params: {grid_search.best_params_}")
+
+    # Retrieve the best model from GridSearchCV
+    best_model = grid_search.best_estimator_
+
+    return best_model
+
+
+if __name__ == "__main__":
+    X_train = pd.read_csv("../data/X_train.csv")
+    y_train = pd.read_csv("../data/y_train.csv").squeeze()
+
+    best_model = train_model(X_train, y_train)
+
+    joblib.dump(best_model, "../models/best_model_rf.pkl")
+
+    print("Best model saved as best_model_rf.pkl")
+
+
+
